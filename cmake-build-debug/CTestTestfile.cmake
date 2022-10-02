4361f0f8d8dab8ef361cbf38efdfa567110fa22b
Jollu <zholu8@gmail.com>
2022-10-02 23:07:20 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jolluemil/Desktop/cgitaction
-# Build directory: /Users/jolluemil/Desktop/cgitaction/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_version "test_version")
-set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/Users/jolluemil/Desktop/cgitaction/CMakeLists.txt;113;add_test;/Users/jolluemil/Desktop/cgitaction/CMakeLists.txt;0;")
--- cmake-build-debug/version.h
+++
@@ -1,7 +0,0 @@
-//
-// Created by Jollu Emil on 10/2/22.
-//
-
-#pragma once
-
-#define PROJECT_VERSION_PATCH 1
